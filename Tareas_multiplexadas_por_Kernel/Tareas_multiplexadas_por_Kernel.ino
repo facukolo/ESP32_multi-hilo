084ebde74c5722f3e789cad46c75b95187d2dbe7
facukolo <133943035+facukolo@users.noreply.github.com>
2023-10-02 19:39:52 -0300

    Add files via upload

--- Tareas_multiplexadas_por_Kernel/Tareas_multiplexadas_por_Kernel.ino
+++ Tareas_multiplexadas_por_Kernel/Tareas_multiplexadas_por_Kernel.ino
@@ -3,8 +3,6 @@
   Se trata de una prueba de código donde se pretende realizar 3 tareas, a demás del loop principal,
   que se ejecutarán en simultáneo, el Kernel del ESP32 se encarga de gestionar la forma de conseguir hacer todas
   esas tareas multiplexando el hilo de proceso entre ellos. En otras palabras, es un "multi-core" gestionado por el Kernel (FreeRTOS).
-  Es interesante saber que en este ejemplo el Kernel decide qué tareas se hacen en cada núcleo, así si detecta que el N0 está fulleado de tareas, asignará la nueva 
-  tarea al otro núcleo. De este modo y por ejemplificar, si queremos hacer tres tareas, sería importante mandar la más pesada a un núcleo especifico y las otras dos dejar en automático.
   ref.: https://www.youtube.com/watch?v=Vus7HE3wc6A&list=PL-Hb9zZP9qC48GcXj_BsDipCPAzwcps6e&index=4&ab_channel=EasyLearning
 */
 #define LEDw 23 
@@ -18,8 +16,8 @@
 //********************************
 //****Definición de las tareas****
 TaskHandle_t blanco;//identificador de tarea
-TaskHandle_t rojo;  //identificador de tarea
-TaskHandle_t verde; //identificador de tarea
+TaskHandle_t rojo;//identificador de tarea
+TaskHandle_t verde;//identificador de tarea
 void crearTareas(void);
 void TaskW(void* pvParameters);//tarea
 void TaskR(void* pvParameters);//tarea
@@ -28,11 +26,11 @@ void TaskG(void* pvParameters);//tarea
 //******Definición del setup******
 void setup() {
   Serial.begin(9600);
-  pinMode(LEDw,OUTPUT);  //configura el pin LEDw como salida
+  pinMode(LEDw,OUTPUT);//configura el pin LEDw como salida
   digitalWrite(LEDw,LOW);//inicializa el pin en LOW
-  pinMode(LEDr,OUTPUT);  //configura el pin LEDr como salida
+  pinMode(LEDr,OUTPUT);//configura el pin LEDr como salida
   digitalWrite(LEDw,LOW);//inicializa el pin en LOW
-  pinMode(LEDg,OUTPUT);  //configura el pin LEDg como salida
+  pinMode(LEDg,OUTPUT);//configura el pin LEDg como salida
   digitalWrite(LEDw,LOW);//inicializa el pin en LOW
 
   //*******************************
@@ -50,31 +48,28 @@ void loop() {
 void TaskW(void* pvParameters){
 
   while(1){
-    Serial.println("Núcleo: "+ String(xPortGetCoreID()));
     digitalWrite(LEDw,HIGH);//setea el pin LEDw
-    delay(delayw);          //espera "delayw" segundos
-    digitalWrite(LEDw,LOW); //resetea el  pin LEDw
-    delay(delayw);          //espera "delayw" milisegundos
+    delay(delayw);//espera "delayw" segundos
+    digitalWrite(LEDw,LOW);//resetea el  pin LEDw
+    delay(delayw);//espera "delayw" segundos
   }
 }
 void TaskR(void* pvParameters){
 
   while(1){
-    Serial.println("Núcleo: "+ String(xPortGetCoreID()));
     digitalWrite(LEDr,HIGH);//setea el pin LEDr
-    delay(delayr);          //espera "delayr" milisegundos
-    digitalWrite(LEDr,LOW); //resetea el pin LEDr
-    delay(delayr);          //espera "delayr" milisegundos
+    delay(delayr);//espera "delayr" segundos
+    digitalWrite(LEDr,LOW);//resetea el pin LEDr
+    delay(delayr);//espera "delayr" segundos
   }
 }
 void TaskG(void* pvParameters){
 
   while(1){
-    Serial.println("Núcleo: "+ String(xPortGetCoreID()));
     digitalWrite(LEDg,HIGH);//setea el pin LEDg
-    delay(delayg);          //espera "delayg" segundos
-    digitalWrite(LEDg,LOW); //resetea el pin LEDg
-    delay(delayg);          //espera "delayg" milisegundos
+    delay(delayg);//espera "delayg" segundos
+    digitalWrite(LEDg,LOW);//resetea el pin LEDg
+    delay(delayg);//espera "delayg" segundos
   }
 }
 
